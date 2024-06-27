2f9a573e72e23a218931ff27b1b14683db43bff0
Mikhail Kharlamau <mikhail.kharlamau@gmail.com>
2024-06-27 21:07:50 +0200

    1 comit standart z webovej stranky

---
+++ src/stm32f0xx_it.c
@@ -0,0 +1,152 @@
+/**
+  ******************************************************************************
+  * @file    SysTick_Example/stm32f0xx_it.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    17-January-2014
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_it.h"
+#include "stm32f072b_discovery.h"
+
+/** @addtogroup STM32F072B_DISCOVERY_Examples
+  * @{
+  */
+
+/** @addtogroup SysTick_Example
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern __IO uint32_t TimingDelay;
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M0 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+    /* Go to infinite loop when Hard Fault exception occurs */
+    while (1)
+    {
+    }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+    static uint16_t tick = 0;
+    static uint8_t  blue_state=0;
+
+    switch (tick++)
+    {
+        // orange blink via low level peripheral library
+        case 150:
+            tick = 0;
+            GPIOC->ODR ^= (1 << 8);
+            break;
+        case 100:
+            if(blue_state)
+                GPIOC->BRR = 0x0080;
+            else
+                GPIOC->BSRR = 0x0080;
+            blue_state ^= 1;
+            break;
+        case 50:
+            // green blink via higher level board specific library
+            STM_EVAL_LEDToggle(LED5);
+            //STM_EVAL_LEDOn(LED5);
+            break;
+    }
+}
+
+
+/******************************************************************************/
+/*                 STM32F0xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f0xx.s).                                            */
+/******************************************************************************/
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
